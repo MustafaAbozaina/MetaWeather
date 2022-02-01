87cbf9bba076a9af46863980624ec04f4965ae98
mustafa salah eldin <programmermostafa1@gmail.com>
2022-02-01 23:14:46 +0200

    Initial Commit

---
+++ MetaWeatherFeed/MetaWeatherFeed.h
@@ -0,0 +1,18 @@
+//
+//  MetaWeatherFeed.h
+//  MetaWeatherFeed
+//
+//  Created by mustafa salah eldin on 2/1/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MetaWeatherFeed.
+FOUNDATION_EXPORT double MetaWeatherFeedVersionNumber;
+
+//! Project version string for MetaWeatherFeed.
+FOUNDATION_EXPORT const unsigned char MetaWeatherFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MetaWeatherFeed/PublicHeader.h>
+
+
