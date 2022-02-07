a7bff9498de9bcc1572d936fd0efd8cd6857e706
mustafa salah eldin <programmermostafa1@gmail.com>
2022-02-07 23:29:02 +0200

    remove duplication of creating sut and url session for each test case

--- MetaWeatherFeedTests/APIInfra/NetworkManagerTests.swift
+++ MetaWeatherFeedTests/APIInfra/NetworkManagerTests.swift
@@ -19,20 +19,24 @@ class NetworkManagerTests: XCTestCase {
     }
 
     func test_getFromUrl_shouldFailInCaseStatusCodeIsGreaterThan299(){
-        let urlSession = URLSessionSpy()
+        let (sut, urlSession) = makeSUT()
         let data = Data()
         let failedStatusCodes = [300, 301, 400, 401, 402, 403, 404, 500]
         failedStatusCodes.forEach { number in
             let response = HTTPURLResponse(url: anyURL(), statusCode: number, httpVersion: nil, headerFields: nil)!
             urlSession.completions.append( (HTTPClient.Result {return (data, response)}))
         }
-        
-        let networkManager = NetworkManager(httpClient: urlSession)
-        networkManager.get(url: anyStringUrl, httpMethod: .get, parameters: nil) { (a:DecodableTest) in
+        sut.get(url: anyStringUrl, httpMethod: .get, parameters: nil) { (a:DecodableTest) in
             XCTFail()
         } failure: { error in
            XCTAssertNotNil(error)
         }
+    }
+    
+    func makeSUT() -> (NetworkManager, URLSessionSpy) {
+        let urlSession = URLSessionSpy()
+        let networkManager = NetworkManager(httpClient: urlSession)
+        return (networkManager, urlSession)
     }
     
 }
